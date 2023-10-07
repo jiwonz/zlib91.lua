916fea70e2ffccac62d52b1931941b3b3fe80358
jiwonz <53703001+jiwonz@users.noreply.github.com>
2023-10-07 23:47:56 +0900

    Update and rename init.luau to init.lua

--- rbx/zlib91/init.lua
+++ rbx/zlib91/init.lua
@@ -3,15 +3,17 @@ local module = {}
 local Compression = require(script.Compression)
 local Base91 = require(script.Base91)
 
+local mode = {
+    level = level or 6;
+    strategy = strategy or "dynamic"
+}
+
 local function toSingleQuoto(str:string)
     return string.gsub(str,"\"","'")
 end
 
 function module.compress(data:string,useSingleQuote:boolean,level:number,strategy:"dynamic"|"fixed"|"huffman_only"):string
-    local compressed = Compression.Zlib.Compress(data,{
-        level = level or 6;
-        strategy = strategy or "dynamic"
-    })
+    local compressed = Compression.Zlib.Compress(data,mode)
     local base91Encoded = Base91.encode(compressed)
     return not useSingleQuote and base91Encoded or toSingleQuoto(base91Encoded)
 end
