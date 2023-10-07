647c96d51dd490bc8c22bc80bb5dd45ad96849bc
jiwonz <53703001+jiwonz@users.noreply.github.com>
2023-10-07 23:51:14 +0900

    rollback bruh

--- rbx/zlib91/init.lua
+++ rbx/zlib91/init.lua
@@ -3,17 +3,15 @@ local module = {}
 local Compression = require(script.Compression)
 local Base91 = require(script.Base91)
 
-local mode = {
-    level = level or 6;
-    strategy = strategy or "dynamic"
-}
-
 local function toSingleQuoto(str:string)
     return string.gsub(str,"\"","'")
 end
 
 function module.compress(data:string,useSingleQuote:boolean,level:number,strategy:"dynamic"|"fixed"|"huffman_only"):string
-    local compressed = Compression.Zlib.Compress(data,mode)
+    local compressed = Compression.Zlib.Compress(data,{
+        level = level or 6;
+        strategy = strategy or "dynamic"
+    })
     local base91Encoded = Base91.encode(compressed)
     return not useSingleQuote and base91Encoded or toSingleQuoto(base91Encoded)
 end
