08edd63c4a0c9d8dabbd9a17a2c7cd4783f7a3cd
jiwonz <53703001+jiwonz@users.noreply.github.com>
2023-10-07 23:51:56 +0900

    rollback bruh sorry

--- lua/zlib91.lua
+++ lua/zlib91.lua
@@ -2,17 +2,16 @@ local module = {}
 
 local compression = require("lib.compression")
 local base91 = require("lib.base91")
-local mode = {
-    level = level or 6;
-    strategy = strategy or "dynamic"
-}
 
 local function toSingleQuoto(str)
     return string.gsub(str,"\"","'")
 end
 
 function module.compress(data,useSingleQuote,level,strategy)
-    local compressed = compression.Zlib.Compress(data,mode)
+    local compressed = compression.Zlib.Compress(data,{
+        level = level or 6;
+        strategy = strategy or "dynamic"
+    })
     local base91Encoded = base91.encode(compressed)
     return not useSingleQuote and base91Encoded or toSingleQuoto(base91Encoded)
 end
